647776e1f8a7606d1683d92361b93b3286c8bcdd
Arvin Bonggal <arvin.bonggal@gmail.com>
2025-03-22 13:50:07 +0800

    My code challenge answer

--- src/index.js
+++ src/index.js
@@ -14,9 +14,55 @@ function App() {
   );
 }
 
-function Avatar() {}
-function Intro() {}
-function Skillset() {}
+// Avatar Component
+function Avatar() {
+  return (
+    <div>
+      <img className="avatar" src="./my-profile.jpg" alt="My Profile" />
+    </div>
+  );
+}
+
+// Intro Component
+function Intro() {
+  return (
+    <div>
+      <p>
+        Aspiring full-stact web developer and solutions architect for
+        applications and databases at Shellsoft Technology Corporation, I love
+        playing MMORPG games, building Gunpla, and watching anime.
+      </p>
+    </div>
+  );
+}
+
+// Skillset Component
+function Skillset() {
+  return (
+    <div>
+      <ul className="skill-list">
+        <li className="skill" style={{ backgroundColor: "blue" }}>
+          HTML+CSS &#128516;
+        </li>
+        <li className="skill" style={{ backgroundColor: "Yellow" }}>
+          JavaScript &#128507;
+        </li>
+        <li className="skill" style={{ backgroundColor: "Green" }}>
+          Python &#128508;
+        </li>
+        <li className="skill" style={{ backgroundColor: "Gray" }}>
+          Git and GitHub &#128509;
+        </li>
+        <li className="skill" style={{ backgroundColor: "orange" }}>
+          Terraform &#128511;
+        </li>
+        <li className="skill" style={{ backgroundColor: "orange" }}>
+          React &#128515;
+        </li>
+      </ul>
+    </div>
+  );
+}
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
