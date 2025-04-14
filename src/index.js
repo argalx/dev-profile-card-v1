76e807eb66e0feecc75192edd51aa80181098dc8
Arvin Bonggal <arvinbonggal@gmail.com>
2025-04-14 16:59:01 +0800

    Challenge No.2 Final Answer

--- src/index.js
+++ src/index.js
@@ -2,6 +2,57 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./style.css";
 
+// Task 1: Create an list/array of skills (skills, level (Beginner (Baby), Intermediate (Like), Advanced (Bicep)), color)
+const skills = [
+  {
+    skill: "HTML+CSS",
+    level: "Advanced",
+    color: "blue",
+  },
+  {
+    skill: "JavaScript",
+    level: "Intermediate",
+    color: "yellow",
+  },
+  {
+    skill: "Python",
+    level: "Beginner",
+    color: "green",
+  },
+  {
+    skill: "React",
+    level: "Intermediate",
+    color: "gray",
+  },
+  {
+    skill: "Git and Github",
+    level: "Intermediate",
+    color: "orange",
+  },
+  {
+    skill: "Terraform",
+    level: "Intermediate",
+    color: "purple",
+  },
+  {
+    skill: "ARM Template/Bicep",
+    level: "Intermediate",
+    color: "pink",
+  },
+  {
+    skill: "TypeScript",
+    level: "Beginner",
+    color: "blue",
+  },
+  {
+    skill: "SQL",
+    level: "Advanced",
+    color: "Orange",
+  },
+];
+
+// Task 3: Display certain emoji for each skill level
+
 function App() {
   return (
     <div className="card">
@@ -38,24 +89,37 @@ function Intro() {
 
 // Skillset Component
 function Skillset() {
+  // Task 2: Loop through the skills and display them
+  const skillObject = skills;
+
   return (
     <div className="skill-list">
-      <Skill skill="HTML+CSS" emoji="&#128507;" color="blue" />
-      <Skill skill="JavaScript" emoji="&#128508;" color="yellow" />
-      <Skill skill="Python" emoji="&#128509;" color="green" />
-      <Skill skill="React" emoji="&#128510;" color="gray" />
-      <Skill skill="Git and Github" emoji="&#128511;" color="orange" />
-      <Skill skill="Terraform" emoji="&#128512;" color="purple" />
-      <Skill skill="ARM Template/Bicep" emoji="&#128513;" color="pink" />
+      {skillObject.map((skill) => (
+        <Skill
+          skill={skill.skill}
+          level={skill.level}
+          color={skill.color}
+          key={skill.skill}
+        />
+      ))}
     </div>
   );
 }
 
-function Skill(props) {
+function Skill({ skill, level, color }) {
+  console.log(skill, level, color);
+
   return (
-    <div className="skill" style={{ backgroundColor: props.color }}>
-      <span>{props.skill}</span>
-      <span>{props.emoji}</span>
+    <div className="skill" style={{ backgroundColor: color }}>
+      <span>{skill}</span>
+      {/* {level === "Beginner" && <span>👶</span>}
+      {level === "Intermediate" && <span>👍</span>}
+      {level === "Advanced" && <span>💪</span>} */}
+      <span>
+        {level === "Beginner" && "👶"}
+        {level === "Intermediate" && "👍"}
+        {level === "Advanced" && "💪"}
+      </span>
     </div>
   );
 }
